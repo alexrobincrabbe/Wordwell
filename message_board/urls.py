4ab77bf02d81c6a405f0036216b97ebfe88760bb
Alexander Crabbe <alienzen@hotmail.com>
2024-07-23 19:10:15 +0000

    feat:enable create new post

--- message_board/urls.py
+++ message_board/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('board/', views.MessageBoard.as_view(), name="message_board"),
+    path('board/post', views.new_post, name="new_post"),
     path('board/<slug:slug>', views.view_post, name="view_post"),
 ]
--- message_board/views.py
+++ message_board/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.views.generic import ListView
 from .models import Post
+from .forms import PostForm
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
@@ -16,3 +19,22 @@ def view_post(request,slug):
                 {
                     "post":post,
                 })
+
+def new_post(request):
+    if request.method == 'POST':
+        new_post= PostForm(data=request.POST)
+        if new_post.is_valid:
+            post= new_post.save(commit=False)
+            post.author = request.user
+            post.save()
+            messages.success(request,f'New post created')
+            return HttpResponseRedirect(reverse('view_post', args=[post.slug]))
+    else:
+        new_post = PostForm()
+    return render (
+        request,
+        'message_board/new_post.html',
+        {
+            'new_post': new_post
+        },
+    )
