73b09f1f9a4aad480007acbb0acfb060ddbbbd69
Alexander Crabbe <alienzen@hotmail.com>
2024-07-23 16:16:44 +0000

    fix: collect static

--- wordwell/settings.py
+++ wordwell/settings.py
@@ -39,12 +39,12 @@ ALLOWED_HOSTS = [
 
 INSTALLED_APPS = [
     'django.contrib.admin',
-    'cloudinary_storage',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
