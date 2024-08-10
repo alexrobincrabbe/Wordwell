0b97dd67f9906d87cac2b2c139f482ed12678c86
Alexander Crabbe <alienzen@hotmail.com>
2024-08-10 12:02:26 +0000

    fix: highscores styles

--- static/js/highscores.js
+++ static/js/highscores.js
@@ -2,5 +2,17 @@ const rankings = document.getElementsByClassName("ranking")
 let rank = 0;
 for (let ranking of rankings){
     rank=rank+1;
-    ranking.innerHTML=rank
+    switch(true){
+        case (rank==1):
+            ranking.innerHTML="1st";
+            break;
+        case (rank==2):
+            ranking.innerHTML="2nd";
+            break;
+        case (rank==3):
+            ranking.innerHTML="3rd";
+            break;
+        default:
+            ranking.innerHTML=`${rank}th`;
+    }   
 }
