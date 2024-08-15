b3436c06c89ef801c008d21bf4ea24013d28a1c0
Alexander Crabbe <alienzen@hotmail.com>
2024-08-15 15:09:44 +0000

    fix: update profile_page tests, cover all cases and add comments

--- profile_page/test_forms.py
+++ profile_page/test_forms.py
@@ -1,20 +1,42 @@
 
 from django.test import TestCase
-from .forms import ProfileUpdateForm
+from .forms import ProfileUpdateForm, UserUpdateForm
 
 
 class TestProfileUpdateForm(TestCase):
 
-    def test_form_is_valid(self):
+    def test_profile_form_is_valid(self):
         """
-        Test for non-mandatory fields
+        Test is valid with mandatory field display name completed
         """
         form = ProfileUpdateForm({'display_name':'jack'})
         self.assertTrue(form.is_valid(), msg='Display name was provided, but form is not valid')
 
-    def test_form_is_not_valid(self):
+
+    def test_profile_form_is_not_valid(self):
         """
-        Test for display_name field
+        Test is not valid without display_name field
         """
         form = ProfileUpdateForm({'about_me': 'Blah Blah Blah','display_name':''})
         self.assertFalse(form.is_valid(), msg='Display name was not provided, but the form is valid')
+
+    def test_user_form_is_valid_without_email(self):
+        """
+        Test is valid with no email
+        """
+        form = UserUpdateForm({'email':''})
+        self.assertTrue(form.is_valid(), msg='Email was blank, but form is not valid')
+
+    def test_user_form_is_valid_with_email(self):
+        """
+        Test is valid with valid email
+        """
+        form = UserUpdateForm({'email':'jack@gmail.com'})
+        self.assertTrue(form.is_valid(), msg='Email was valid, but form is not valid')
+
+    def test_user_form_is_not_valid(self):
+        """
+        Test is not valid with invalid email
+        """
+        form = UserUpdateForm({'email': 'invadidEmail'})
+        self.assertFalse(form.is_valid(), msg='Email was invalid, but the form is valid')
--- profile_page/test_views.py
+++ profile_page/test_views.py
@@ -2,9 +2,15 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from django.shortcuts import reverse
 from .models import UserProfile
-import datetime
 
 class TestProfilePageViews(TestCase):
+    '''
+    Tests that:
+    page returns status of 200 and success message when profile is updated
+    page does not display success message if the form is not valid on submission
+    View profile page contains all of the correct profile information
+    
+    '''
     
     def setUp(self):
         """
@@ -25,19 +31,69 @@ class TestProfilePageViews(TestCase):
             )
         self.profile[0].save()
 
-    def test_update_profile(self):
+    def test_update_profile_with_display_name_success(self):
+        #user must be logged in to update profile
         self.client.login(
             username="myUsername", password ="myPassword")
+        #email is blank and display name is completed
         post_data = {
-            'email':'myEmail',
+            'email':'',
             'display_name':'myDisplayName',
+            'about_me':'',
+        }
+        response = self.client.post(reverse('update_profile'), post_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(
+            b'Your account has been updated', response.content, msg="Form was valid but success message was not displayed"
+        )
+
+    def test_update_profile_with_valid_email(self):
+        #user must be logged in to update profile
+        self.client.login(
+            username="myUsername", password ="myPassword")
+        #email is valid and display name is completed
+        post_data = {
+            'email':'jack@email.com',
+            'display_name':'myDisplayName',
+            'about_me':'',
+        }
+        response = self.client.post(reverse('update_profile'), post_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(
+            b'Your account has been updated', response.content, msg="Form was valid but success message was not displayed"
+        )
+
+    def test_update_profile_without_display_name(self):
+        #user must be logged in to update profile
+        self.client.login(
+            username="myUsername", password ="myPassword")
+        #Display name is blank
+        post_data = {
+            'email':'email@gmail.com',
+            'display_name':'',
             'about_me':'aboutMeText',
         }
         response = self.client.post(reverse('update_profile'), post_data)
         self.assertEqual(response.status_code, 200)
-        #self.assertIn(
-        #    b'Your account has been updated', response.content
-        #)
+        self.assertNotIn(
+            b'Your account has been updated', response.content, msg="Display name was left empty but success message was displayed"
+        )
+
+    def test_update_profile_with_invalid_email(self):
+        #user must be logged in to update profile
+        self.client.login(
+            username="myUsername", password ="myPassword")
+        #Email is invalid
+        post_data = {
+            'email':'invalidemail',
+            'display_name':'Jack',
+            'about_me':'aboutMeText',
+        }
+        response = self.client.post(reverse('update_profile'), post_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertNotIn(
+            b'Your account has been updated', response.content, msg="Email was invalid but success message was displayed"
+        )
 
     def test_view_profile(self):
         """
@@ -45,11 +101,16 @@ class TestProfilePageViews(TestCase):
         Check that page exists
         Check that user and user profile info is displayed on the page
         """
+        #set login date equal to joined date
+        self.user.last_login=self.user.date_joined
+        #format dates as shown on the page
         join_date=self.user.date_joined.strftime("%b. %d, %Y, %-I:%M")
-        
+        last_login=self.user.last_login.strftime("%b. %d, %Y, %-I:%M")
+
         response = self.client.get(reverse('view_profile', args=['myusername']))
-        self.assertEqual(response.status_code, 200) #url exists
-        self.assertIn(b"blah blah blah", response.content) #about_me
-        self.assertIn(b"123456", response.content) #high_score
-        self.assertIn(b"Bucky", response.content) #display_name
-        self.assertIn(bytes(join_date,encoding='utf-8'), response.content)
+        self.assertEqual(response.status_code, 200) #url exists and loads page
+        self.assertIn(b"blah blah blah", response.content, msg="About me was not displayed on profile") #about_me
+        self.assertIn(b"123456", response.content,  msg="High score was not displayed on profile") #high_score
+        self.assertIn(b"Bucky", response.content, msg="Display name was not displayed on profile") #display_name
+        self.assertIn(bytes(join_date,encoding='utf-8'), response.content, msg="Join date was not displayed on profile") #join date
+        self.assertIn(bytes(last_login,encoding='utf-8'), response.content, msg="Last login was not displayed on profile") #login date
