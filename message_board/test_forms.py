5750579188dde75d0c2e3e99cd540e122f2f585e
Alexander Crabbe <alienzen@hotmail.com>
2024-08-17 11:44:41 +0000

    feat: add automatic tests, message board forms

---
+++ message_board/test_forms.py
@@ -0,0 +1,39 @@
+from django.test import TestCase
+from .forms import PostForm, ReplyForm
+
+
+class TestProfileUpdateForm(TestCase):
+
+    def test_post_form_is_valid(self):
+        """
+        Test is valid with mandatory fields name completed
+        """
+        form = PostForm({'title':'testTitle','text':'testText'})
+        self.assertTrue(form.is_valid(), msg='Mandatory fields were provided, but form is not valid')
+
+
+    def test_post_form_is_not_valid(self):
+        """
+        Test is not valid without mandatory fields
+        """
+        form = PostForm({'title': '','text':'testText'})
+        self.assertFalse(form.is_valid(), msg='Title was not provided, but the form is valid')
+        form = PostForm({'title': 'textTitle','text':''})
+        self.assertFalse(form.is_valid(), msg='Text was not provided, but the form is valid')
+    
+    def test_reply_form_is_valid(self):
+        """
+        Test is valid with mandatory fields name completed
+        """
+        form = ReplyForm({'text':'testText'})
+        self.assertTrue(form.is_valid(), msg='Mandatory fields were provided, but form is not valid')
+
+
+    def test_reply_form_is_not_valid(self):
+        """
+        Test is not valid without mandatory fields
+        """
+        form = ReplyForm({'text':''})
+        self.assertFalse(form.is_valid(), msg='Text was not provided, but the form is valid')
+
+    
