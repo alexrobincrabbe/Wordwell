c18429a90a8111ac4932c1f2486e93df8cfa04f7
Alexander Crabbe <alienzen@hotmail.com>
2024-08-18 10:58:54 +0000

    fix: validate, format CSS

--- game/test_views.py
+++ game/test_views.py
@@ -18,18 +18,21 @@ class testGameViews(TestCase):
         )
     
 
-    def test_instructions_view(self):
+    def test_game_view(self):
+        # user must be logged in to save score
         self.client.login(
             username="myUsername", password ="myPassword")
-
-
+        
         response=self.client.get(reverse('game'))
         self.assertEqual(response.status_code, 200)
+
         post_data = {
             'score': 3142,
         }
+        # check score does not exist in database
         score = Scores.objects.filter(score=3142).exists()
         self.assertEqual(score, False)
+        # check score exists after score is sent as post request
         post_response = self.client.post(reverse('game'), post_data)
         self.assertEqual(post_response.status_code, 200)
         score = Scores.objects.filter(score=3142).exists()
